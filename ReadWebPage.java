14088dcae5b5f12a589583ac6e61fdb87d278e8c
megmkr <97320950+megmkr@users.noreply.github.com>
2022-09-06 21:43:01 -0700

    Update ReadWebPage.java
    starting the exporting to file stuff

--- ReadWebPage.java
+++ ReadWebPage.java
@@ -8,13 +8,20 @@ import java.net.http.HttpResponse;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
+import java.lang.String;
 
 public class ReadWebPage 
 {
    public static int startx = 0;
    public static String inputPageUrl;
    public static String htmlContent;
+   
+   private static final String DELIMITER = ",";
+   private static final String SEPARATOR = "\n";
+    
+    //File header
+    private static final String HEADER = "Title,Path,Author,Servings,Ingredients,Procedure";
         
    public static void main(String[] args) throws IOException, InterruptedException {
       ArrayList<ArrayList<String>> recipes = new ArrayList<ArrayList<String>>();
@@ -26,7 +33,7 @@ public class ReadWebPage
       pageUrls.add("https://www.surlatable.com/recipes/?srule=best-matches&start=72&sz=24");
       
       
-      int size1 = 1;
+      int size1 = 3;
       int j=0;
         
       for(int a=0; a<size1; a++)
@@ -47,27 +54,69 @@ public class ReadWebPage
             //get first arraylist and then add recipe url to the filler arraylist 
             recipes.get(j).add(scraper.extractRecipeUrl(htmlContent));
             final String recipeContent = scraper.getRecipeContent(recipes.get(i).get(0));
-            recipes.get(i).add(scraper.extractPath(recipeContent));
-            recipes.get(i).add(scraper.extractTitle(recipeContent));
-            recipes.get(i).add(scraper.extractAuthor(recipeContent));
-            recipes.get(i).add(scraper.extractServings(recipeContent));
-            recipes.get(i).add(scraper.extractIngredients(recipeContent));
-            recipes.get(i).add(scraper.extractProcedure(recipeContent));
+            recipes.get(j).add(scraper.extractPath(recipeContent));
+            recipes.get(j).add(scraper.extractPath(recipeContent));
+            recipes.get(j).add(scraper.extractTitle(recipeContent));
+            recipes.get(j).add(scraper.extractAuthor(recipeContent));
+            recipes.get(j).add(scraper.extractServings(recipeContent));
+            recipes.get(j).add(scraper.extractIngredients(recipeContent));
+            recipes.get(j).add(scraper.extractProcedure(recipeContent));
 
             if(j==23 || j== 47 || j==71)
                startx=0;
           }
         }
         
-      System.out.println(recipes.get(0).get(1));
+      System.out.println(recipes.get(1).get(0));
       /*System.out.println(recipes.get(0).get(2));
       System.out.println(recipes.get(0).get(3));
       System.out.println(recipes.get(0).get(4));
       System.out.println(recipes.get(0).get(5));*/
 
-      //System.out.println(recipes.get().get(0));
+      System.out.println(recipes.get(2).get(0));
       //System.out.println(recipes.get(23).get(0));
       //System.out.println(recipes.get(0).get(1));
+      
+      FileWriter file = null;
+      
+      try{
+        file = new FileWriter("RecipeList.csv");
+        //Add header
+        file.append(HEADER);
+        //Add a new line after the header
+        file.append(SEPARATOR);
+        String toAdd = "";
+        for(int a=0; a<size1; a++)
+        {
+         for(int b=1; b<8; b++){
+            toAdd = recipes.get(a).get(b);
+            
+            toAdd = toAdd.replaceAll("<li>","");
+            toAdd = toAdd.replaceAll("</li>","");
+            toAdd = toAdd.replaceAll("<br>","");
+            toAdd = toAdd.replaceAll("<b>","");
+            toAdd = toAdd.replaceAll("</b>","");
+            toAdd = toAdd.replaceAll("</ul>","");
+            toAdd = toAdd.replaceAll("<i>","");
+            toAdd = toAdd.replaceAll("</i>","");
+            toAdd = toAdd.replaceAll("&#188;","¼");
+            toAdd = toAdd.replaceAll("&#189;","½");
+            toAdd = toAdd.replaceAll("&#8539;","⅛");
+            toAdd = toAdd.replaceAll("&#176;","°");
+            file.append(toAdd);
+            file.append(DELIMITER);
+
+         } 
+            file.append(SEPARATOR);      
+            
+        
+        }
+        file.close();
+   
+      }catch(Exception e)
+      {
+        e.printStackTrace();
+      }
    }
         
    
@@ -162,10 +211,10 @@ public class ReadWebPage
     
     private String extractPath(String content)
     {
-      final Pattern servingsRegExp = Pattern.compile("title=\"Go to(.*?)\">", Pattern.DOTALL);
-      final Matcher matcher = servingsRegExp.matcher(content);
-      matcher.find();
-      String path = "Home/Recipes/" +matcher.group(1) + "/";
+      //final Pattern servingsRegExp = Pattern.compile("title=\"Go to(.*?)\">", Pattern.DOTALL);
+      //final Matcher matcher = servingsRegExp.matcher(content);
+      //matcher.find();
+      String path = "Home/Recipes/" ;
       //servingsRegExp = Pattern.compile("\"primaryCategory\":\"(.*?)\"", Pattern.DOTALL);
    
       return path;
